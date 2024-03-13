218d959d14828ffb8a178ff2414e85cbf93756f9
Lalika31253 <Lalika31253@gmail.com>
2024-03-12 21:23:00 -0600

    Practicing with class-based components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,88 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: 'Lukas Souz'
+  }
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = {
+    loading: false,
+    focused: null
 
-    return <main className={dashboardClasses} />;
+  };
+ /* Instance Method */
+  selectPanel(id) {
+    // this.setState({
+    //   focused: id
+    // });
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
+
+  /* Class Property with Arrow Function */
+  // selectPanel = id => {
+  //   this.setState({
+  //    focused: id
+  //   });
+  // };
+
+
+   /* Class Property with Function expression */
+  //  selectPanel = function(id) {
+  //   this.setState({
+  //    focused: id
+  //   });
+  // };
+
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          // onSelect={this.selectPanel}
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ));
+
+    return <main className={dashboardClasses}>{panels}</main>
+
   }
 }
 
